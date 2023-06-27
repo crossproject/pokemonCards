cfcb4dede6dc300208cc72887ad0c180741dd9c8
farah-st <farah.perezcarrillo@gmail.com>
2023-06-27 17:21:52 -0600

    modifications

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,19 +1,15 @@
-// wrapper for querySelector...returns matching element
 export function qs(selector, parent = document) {
     return parent.querySelector(selector);
   }
-  // or a more concise version if you are into that sort of thing:
-  // export const qs = (selector, parent = document) => parent.querySelector(selector);
-  
-  // retrieve data from localstorage
+
   export function getLocalStorage(key) {
     return JSON.parse(localStorage.getItem(key));
   }
-  // save data to local storage
+
   export function setLocalStorage(key, data) {
     localStorage.setItem(key, JSON.stringify(data));
   }
-  // set a listener for both touchend and click
+
   export function setClick(selector, callback) {
     qs(selector).addEventListener("touchend", (event) => {
       event.preventDefault();
@@ -61,7 +57,6 @@ export function qs(selector, parent = document) {
   }
   
   function loadTemplate(path) {
-    // wait what?  we are returning a new function? this is called currying and can be very helpful.
     return async function () {
       const res = await fetch(path);
       if (res.ok) {
@@ -72,9 +67,6 @@ export function qs(selector, parent = document) {
   }
   
   export async function loadHeaderFooter() {
-    // header template will still be a function! But one where we have pre-supplied the argument.
-    // headerTemplate and footerTemplate will be almost identical, but they will remember the path we passed in when we created them
-    // why is it important that they stay functions?  The renderWithTemplate function is expecting a template function...if we sent it a string it would break, if we changed it to expect a string then it would become less flexible.
     const headerTemplateFn = loadTemplate("/partials/header.html");
     const footerTemplateFn = loadTemplate("/partials/footer.html");
     const headerEl = document.querySelector("#main-header");
