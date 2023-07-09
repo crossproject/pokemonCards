a3fff969ba3d7c97f59b171dd5665829f7a02857
farah-st <farah.perezcarrillo@gmail.com>
2023-07-09 08:26:21 -0600

    adding more cards

--- src/js/pokemonCard.js
+++ src/js/pokemonCard.js
@@ -3,5 +3,11 @@ import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
-const pokemonId = 150;
-pokemonDetails(pokemonId);
+const pokemonIdsGroup1 = [150, 151, 152]; // First group of Pokemon IDs
+const pokemonIdsGroup2 = [25, 26, 27]; // Second group of Pokemon IDs
+
+const pokemonIdsArray = [pokemonIdsGroup1, pokemonIdsGroup2]; // Array of arrays
+
+for (const pokemonIds of pokemonIdsArray) {
+  pokemonDetails(pokemonIds);
+}
--- src/js/pokemonCards.mjs
+++ src/js/pokemonCards.mjs
@@ -1,67 +1,73 @@
-import { getPokemonByPokedexNumber} from "./externalSevices.mjs";
-import { renderWithTemplate } from "./utils.mjs";
+import { getPokemonByPokedexNumber } from "./externalSevices.mjs";
 
-// Main function
-export default async function pokemonDetails(pokemonId) {
-    const pokemon =   await getPokemonByPokedexNumber(pokemonId);
-          if (pokemon === undefined) {
-        throw Error();        
+export default async function pokemonDetails(pokemonIds) {
+  try {
+    const container = document.querySelector(".pokemon-cards");
+
+    for (const pokemonId of pokemonIds) {
+      const pokemon = await getPokemonByPokedexNumber(pokemonId);
+      if (pokemon === undefined) {
+        console.error(`Pokemon with ID ${pokemonId} not found.`);
+        continue;
       }
-      const pokemonDetails = document.querySelector(".pokemon-card");
-      await renderWithTemplate(pokemonDetailsTemplate,pokemonDetails,pokemon);
-
-    };
-
-
-export function pokemonDetailsTemplate(pokemon){
-    return `
-            <h1 id="pokemonName"> ${pokemon.name}</h1>
-            <img id="pokemonImage" src="${pokemon.sprites.front_default}">
-            <p id="pokemonNumber">Dex number: ${pokemon.id}</p>
-            <p id="regionOfOrigin">Region: ${getRegion(pokemon)}</p>
-            <p id="pokemonHeight">Height: ${pokemon.height / 10} m</p>
-            <p id="pokemonWeight">Weight: ${pokemon.weight / 10} kg</p>
-            <p id="pokemonType1">Type: ${pokemon.types[0].type.name} ${type2(pokemon)}</p>
-            <p id="pokemonAbility">Ability: ${pokemon.abilities[0].ability.name}</p>
-            <p id="statTotal">Base Stat total: ${baseStatTotal(pokemon)}</p>
-            `
-  };
-
-
-  function type2(pokemon) {
-    if (pokemon.types[1] != undefined ){
-      return `${pokemon.types[1].type.name}`;
+      const template = generatePokemonDetailsTemplate(pokemon);
+      renderPokemonDetails(template, container);
     }
-    else {
-      return "";
-    }
+  } catch (error) {
+    console.error(error);
   }
+}
 
-  function baseStatTotal(pokemon){
-    let total = 0;
-      for(let i = 0; i < pokemon.stats.length; i++) {
-        total += pokemon.stats[i].base_stat;
-      }
+export function generatePokemonDetailsTemplate(pokemon) {
+  return `
+    <div class="pokemon-card">
+      <h1 id="pokemonName"> ${pokemon.name}</h1>
+      <img id="pokemonImage" src="${pokemon.sprites.front_default}">
+      <p id="pokemonNumber">Dex number: ${pokemon.id}</p>
+      <p id="regionOfOrigin">Region: ${getRegion(pokemon)}</p>
+      <p id="pokemonHeight">Height: ${pokemon.height / 10} m</p>
+      <p id="pokemonWeight">Weight: ${pokemon.weight / 10} kg</p>
+      <p id="pokemonType1">Type: ${pokemon.types[0].type.name} ${type2(pokemon)}</p>
+      <p id="pokemonAbility">Ability: ${pokemon.abilities[0].ability.name}</p>
+      <p id="statTotal">Base Stat total: ${baseStatTotal(pokemon)}</p>
+    </div>
+  `;
+}
+
+export function renderPokemonDetails(template, container) {
+  const card = document.createElement("div");
+  card.innerHTML = template;
+  container.appendChild(card);
+}
+
+function type2(pokemon) {
+  if (pokemon.types[1] !== undefined) {
+    return `${pokemon.types[1].type.name}`;
+  } else {
+    return "";
+  }
+}
+
+function baseStatTotal(pokemon) {
+  let total = 0;
+  for (let i = 0; i < pokemon.stats.length; i++) {
+    total += pokemon.stats[i].base_stat;
+  }
   return total;
-  }
+}
 
-  function getRegion(pokemon){
-    if (pokemon.id < 152) {
-      return "Kanto";
-    }
-    else if (pokemon.id > 151 && pokemon.id < 252) {
-      return "Johto";
-    }
-    else if (pokemon.id > 251 && pokemon.id < 387) {
-      return "Hoenn";
-    }
-    else if (pokemon.id > 386 && pokemon.id < 494) {
-      return "Sinnoh";
-    }
-    else if (pokemon.id > 493 && pokemon.id < 650) {
-      return "Unova";
-    }
-    else if (pokemon.id > 649 && pokemon.id < 722) {
+function getRegion(pokemon) {
+  if (pokemon.id < 152) {
+    return "Kanto";
+  } else if (pokemon.id > 151 && pokemon.id < 252) {
+    return "Johto";
+  } else if (pokemon.id > 251 && pokemon.id < 387) {
+    return "Hoenn";
+  } else if (pokemon.id > 386 && pokemon.id < 494) {
+    return "Sinnoh";
+  } else if (pokemon.id > 493 && pokemon.id < 650) {
+    return "Unova";
+  } else if (pokemon.id > 649 && pokemon.id < 722) {
       return "Kalos";
     }
     else if (pokemon.id > 721 && pokemon.id < 810) {
