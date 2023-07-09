20aa16f737e63b1cc70f74e993d7574ddc8a7525
farah-st <farah.perezcarrillo@gmail.com>
2023-07-09 11:08:13 -0600

    css

--- src/js/pokemonCard.js
+++ src/js/pokemonCard.js
@@ -3,10 +3,11 @@ import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
-const pokemonIdsGroup1 = [150, 151, 152]; // First group of Pokemon IDs
-const pokemonIdsGroup2 = [25, 26, 27]; // Second group of Pokemon IDs
+const pokemonIdsGroup1 = [150, 151, 152, 1]; 
+const pokemonIdsGroup2 = [25, 26, 27, 10]; 
+const pokemonIdsGroup3 = [100, 30, 2, 40]
 
-const pokemonIdsArray = [pokemonIdsGroup1, pokemonIdsGroup2]; // Array of arrays
+const pokemonIdsArray = [pokemonIdsGroup1, pokemonIdsGroup2, pokemonIdsGroup3]; // Array of arrays
 
 for (const pokemonIds of pokemonIdsArray) {
   pokemonDetails(pokemonIds);
