c87a1dc4722c4068a428397ec955e202155e0c86
farah-st <farah.perezcarrillo@gmail.com>
2023-07-14 20:39:53 -0600

    select region added all items

--- src/js/pokemonCards.mjs
+++ src/js/pokemonCards.mjs
@@ -3,7 +3,6 @@ import { getPokemonByPokedexNumber } from "./externalSevices.mjs";
 export default async function pokemonDetails(pokemonIds) {
   try {
     const container = document.querySelector(".pokemon-cards");
-    const filterInput = document.getElementById("filterInput");
 
     for (const pokemonId of pokemonIds) {
       const pokemon = await getPokemonByPokedexNumber(pokemonId);
@@ -15,27 +14,24 @@ export default async function pokemonDetails(pokemonIds) {
       renderPokemonDetails(template, container);
     }
 
-    filterInput.addEventListener("input", applyFilters);
+    const regionMons =  getParam("pokemon");
+    if (regionMons != null) {
+      document.getElementById("chooseRegion").value = regionMons;
+    }
+    console.log(document.getElementById("chooseRegion").selectedIndex)
+    document.getElementById("chooseRegion").addEventListener("change", selectRegion);
+
   } catch (error) {
     console.error(error);
   }
 }
 
-function applyFilters() {
-  const filterValue = filterInput.value.toLowerCase().trim();
-  const pokemonCards = document.querySelectorAll(".pokemon-card");
 
-  pokemonCards.forEach((card) => {
-    const pokemonName = card.querySelector("#pokemonName").textContent.toLowerCase();
-
-    const nameMatch = pokemonName.includes(filterValue);
-
-    if (nameMatch) {
-      card.style.display = "block";
-    } else {
-      card.style.display = "none";
-    }
-  });
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const pokemon = urlParams.get(param);
+  return pokemon;
 }
 
 export function generatePokemonDetailsTemplate(pokemon) {
@@ -61,6 +57,66 @@ export function renderPokemonDetails(template, container) {
   container.appendChild(card);
 }
 
+export function selectRegion() {
+  let startId = 0; 
+  let endId = 0; 
+  
+  // Clear the existing pokemon cards
+  document.querySelector(".pokemon-cards").innerHTML = "";
+
+  if (this == undefined || this.value == "All") {
+
+    startId = 1; 
+    endId = 1010; 
+    
+  } else {
+    // Display specific region based on the selected value
+    if (this.value == "Kanto"){
+      startId = 1; 
+      endId = 151; 
+    }
+    else if (this.value == "Johto"){
+      startId = 152;
+      endId = 251;
+    }
+    else if (this.value == "Hoenn"){
+      startId = 252;
+      endId = 386;
+    }
+    else if (this.value == "Sinnoh"){
+      startId = 387;
+      endId = 493;
+    }
+    else if (this.value == "Unova"){
+      startId = 494;
+      endId = 649;
+    }
+    else if (this.value == "Kalos"){
+      startId = 650;
+      endId = 721;
+    }
+    else if (this.value == "Alola"){
+      startId = 722;
+      endId = 809;
+    }
+    else if (this.value == "Galar"){
+      startId = 810;
+      endId = 905;
+    }
+    else if (this.value == "Hisui"){
+      startId = 810;
+      endId = 905;
+    }
+    else if (this.value == "Paldea"){
+      startId = 906;
+      endId = 1010;
+    }
+  }
+  
+  const pokemonIds = Array.from({ length: endId - startId + 1 }, (_, index) => startId + index);
+  pokemonDetails(pokemonIds);
+}
+
 function type2(pokemon) {
   if (pokemon.types[1] !== undefined) {
     return `${pokemon.types[1].type.name}`;
@@ -77,6 +133,8 @@ function baseStatTotal(pokemon) {
   return total;
 }
 
+
+
 function getRegion(pokemon) {
   if (pokemon.id < 152) {
     return "Kanto";
@@ -89,60 +147,15 @@ function getRegion(pokemon) {
   } else if (pokemon.id > 493 && pokemon.id < 650) {
     return "Unova";
   } else if (pokemon.id > 649 && pokemon.id < 722) {
-    return "Kalos";
-  } else if (pokemon.id > 721 && pokemon.id < 810) {
-    return "Alola";
-  } else if (pokemon.id > 809 && pokemon.id < 906) {
-    return "Galar/Hisui";
-  } else if (pokemon.id > 905 && pokemon.id < 1010) {
-    return "Paldea";
+      return "Kalos";
+    }
+    else if (pokemon.id > 721 && pokemon.id < 810) {
+      return "Alola";
+    }
+    else if (pokemon.id > 809 && pokemon.id < 906) {
+      return "Galar/Hisui";
+    }
+    else if (pokemon.id > 905 && pokemon.id < 1010) {
+      return "Paldea";
+    }
   }
-}
-
-export function selectRegion() {
-  let startId = 0;
-  let endId = 0;
-  const regionMons = getParam("pokemon");
-
-  if (regionMons == "Kanto") {
-    startId = 1;
-    endId = 151;
-  } else if (regionMons == "Johto") {
-    startId = 152;
-    endId = 251;
-  } else if (regionMons == "Hoenn") {
-    startId = 252;
-    endId = 386;
-  } else if (regionMons == "Sinnoh") {
-    startId = 387;
-    endId = 493;
-  } else if (regionMons == "Unova") {
-    startId = 494;
-    endId = 649;
-  } else if (regionMons == "Kalos") {
-    startId = 650;
-    endId = 721;
-  } else if (regionMons == "Alola") {
-    startId = 722;
-    endId = 809;
-  } else if (regionMons == "Galar") {
-    startId = 810;
-    endId = 905;
-  } else if (regionMons == "Hisui") {
-    startId = 810;
-    endId = 905;
-  } else if (regionMons == "Paldea") {
-    startId = 906;
-    endId = 1010;
-  }
-
-  const pokemonIds = Array.from({ length: endId - startId + 1 }, (_, index) => startId + index);
-  pokemonDetails(pokemonIds);
-}
-
-export function getParam(param) {
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const pokemon = urlParams.get(param);
-  return pokemon;
-}
