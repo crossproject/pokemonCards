b7037da7cb908946b9b2e44f2605655f8ecd03c1
farah-st <farah.perezcarrillo@gmail.com>
2023-07-17 17:42:33 -0600

    push

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -12,5 +12,5 @@ export async function showSlides() {
     slideIndex = 1;
   }
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 4000); // Change slide every 2 seconds
+  setTimeout(showSlides, 4000); // Change slide every 4 seconds
 }
