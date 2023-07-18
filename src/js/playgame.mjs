6718accbbef111172404712b4e1ec79e45a9e194
Daniel Parra <l.d.parra@outlook.com>
2023-07-18 20:03:57 -0300

    changes

--- src/js/playgame.mjs
+++ src/js/playgame.mjs
@@ -2,160 +2,256 @@ import { getRandomPokemon } from "./externalSevices.mjs"
 import { baseStatTotal } from "./pokemonCards.mjs";
 
 
-// Función para comparar los stats y determinar el resultado de la batalla
-function battleResult(playerPokemon, enemyPokemon) {
 
-  // Comparar las elecciones y determinar el resultado
-  if (playerPokemon.stats === enemyPokemon.stats) {
-    // Draw
-    return 0;
-  } else if (
-    playerPokemon.stats > enemyPokemon.stats
-  ) {
-    // Player Win
-    return 1;
+export default async function startNewGame(newStadium=false) {
+  // Main function: Start the game, set the variables
+  // renders the images and buttons
+  let playerScore = 0;
+  let playerLives = 6;
 
-  } else {
-    // Player Lose
-    return 2;    
+  let playerPokemon = {
+    name: "",
+    image: "",
+    stats: 0
+  };
+
+  let enemyPokemon = {
+      name: "",
+      image: "",
+      stats: 0
+  };
+
+  const stadium = document.querySelector("#stadium");
+  displayStadium(stadium, newStadium);
+
+  const gameboard = document.querySelector("#gameboard");
+  displayGameboard();
+
+  const buttonSelector = document.querySelector("#action-buttons");
+  displayActionButtons(buttonSelector);
+  
+  document.querySelector("#battle-result").innerHTML = "";
+
+  async function displayGameboard(){
+  // Renders Pokemons, Lives and Score
+    await getNewPokemon(playerPokemon);
+    await getNewPokemon(enemyPokemon);
+
+    const playerDisplayPokemon = document.querySelector("#player-pokemon");
+    displayPokemon(playerDisplayPokemon,playerPokemon)
+
+    const oponentDisplayPokemon = document.querySelector("#oponent-pokemon");
+    displayPokemon(oponentDisplayPokemon,enemyPokemon,false)
+
+    const lifeSelector = document.querySelector("#player-life");
+    displayLife(lifeSelector,playerLives)
+
+    const scoreSelector = document.querySelector("#player-score");
+    displayScore(scoreSelector,playerScore)
+
   }
-}
-
-function playerActionResult(choice) {
-    switch (choice) {
-        case 0:
-            // Empate
-            break;
-        case 1:
-            // Player Win
-        break;
-        case 2:
-            // Player Lose
-            break;
-    }
-}
-
-export async function getNewPokemon(pokemon) {
-    let newPokemon = await getRandomPokemon();
-    pokemon.name = newPokemon.name;
-    pokemon.image = newPokemon.sprites.front_default;
-    pokemon.stats = baseStatTotal(newPokemon);
-}
-
-// let playerName = "";
-// let playerScore = 0;
-// let playerLives = 6;
-
-
-// export default function startNewGame(){
-
-//     // crear un nuevo jugador
-//     // crear score
-//     // generar lifepoints
-//     // obtener un pokemon
-//     // generar pokemon oponente
-
-
-//     const player = {
-//         name: "Player",
-//         score: 0,
-//         life: 6,
-//         init: async function (name) {
-//             this.name = prompt("Ingresa tu Nombre");
-//         }}
-
-//     player.init();
+  
+  
+  function displayNewGameButton(selector) {
+    // Render New Game button (used in Game Over situation)
+    selector.innerHTML = "";
+    const newGameButton = document.createElement("button");
+    newGameButton.textContent = "New Game";
     
+    newGameButton.addEventListener("click", function(e){
+      e.preventDefault();
+      startNewGame(true);
+    })
+    selector.appendChild(newGameButton);
+    }
 
-        
-// };
+  
 
-// // Definición de variables
+  function displayRestartButton(selector) {
+    // Render Restart button (after battle)
+    selector.innerHTML = "";
+    const restartButton = document.createElement("button");
+    restartButton.textContent = "New Battle";
+    
+    restartButton.addEventListener("click", function(e){
+      e.preventDefault();
+      displayGameboard();
+      selector.innerHTML = "";
+      displayActionButtons(selector);
+    })
+    selector.appendChild(restartButton);
+  }
 
-// // Objeto Pokémon del jugador
-// var playerPokemon = {
-//   name: "Pikachu",
-//   image: "pikachu.png",
-//   stats: {
-//     attack: 50,
-//     defense: 40,
-//     speed: 60
-//   }
-// };
+  
+  function displayActionButtons(selector){
+    // Display player option buttons and
+    // their respective event
+    selector.innerHTML="";
+    const button1 = document.createElement("button");
+    button1.textContent = "Bluey Win";
+    button1.value = 1;
+    button1.addEventListener("click", function(e){
+      e.preventDefault();
+      let result = battleResult(playerPokemon,enemyPokemon);
+      compareResult(result,this.value,selector);
+    });
 
-// // Objeto Pokémon del enemigo
-// var enemyPokemon = {
-//   name: "Charizard",
-//   image: "charizard.png",
-//   stats: {
-//     attack: 70,
-//     defense: 60,
-//     speed: 80
-//   }
-// };
 
-// // Función para comparar los stats y determinar el resultado de la batalla
-// function battleResult(playerChoice) {
-//   // Generar un número aleatorio entre 1 y 3 para simular la elección del enemigo
-//   var enemyChoice = Math.floor(Math.random() * 3) + 1;
+    const button2 = document.createElement("button");
+    button2.textContent = "Reedo Win";
+    button2.value = 2;
+    button2.addEventListener("click", function(e){
+      e.preventDefault();
+      let result = battleResult(playerPokemon,enemyPokemon);
+      compareResult(result,this.value,selector);
+    });
 
-//   // Comparar las elecciones y determinar el resultado
-//   if (playerChoice === enemyChoice) {
-//     // Empate
-//     console.log("Empate. ¡Juega otra ronda!");
-//   } else if (
-//     (playerChoice === 1 && enemyChoice === 3) ||
-//     (playerChoice === 2 && enemyChoice === 1) ||
-//     (playerChoice === 3 && enemyChoice === 2)
-//   ) {
-//     // El jugador gana
-//     var scoreDifference = calculateScoreDifference(playerPokemon.stats, enemyPokemon.stats);
-//     playerScore += scoreDifference;
-//     console.log("¡Ganaste! Sumaste " + scoreDifference + " puntos.");
-//   } else {
-//     // El enemigo gana
-//     var scoreDifference = calculateScoreDifference(enemyPokemon.stats, playerPokemon.stats);
-//     playerScore -= scoreDifference;
-//     playerLives--;
-//     console.log("Perdiste. Restaste " + scoreDifference + " puntos y perdiste una vida.");
-//   }
+    selector.appendChild(button1);
+    selector.appendChild(button2);
+  }
 
-//   // Mostrar el puntaje y vidas del jugador
-//   console.log("Puntaje: " + playerScore);
-//   console.log("Vidas restantes: " + playerLives);
+  function battleResult(playerPokemon, enemyPokemon) {
+    // Compares Pokemon stats
 
-//   // Verificar si el juego ha terminado
-//   if (playerLives <= 0) {
-//     console.log("Fin del juego. ¡Has perdido todas tus vidas!");
-//     // Aquí puedes agregar cualquier acción adicional que desees al finalizar el juego
-//   }
-// }
+    if (playerPokemon.stats === enemyPokemon.stats) {
+      // Draw
+      return 0;
+    } else if (
+      playerPokemon.stats > enemyPokemon.stats
+    ) {
+      // Player Pokemon Wins
+      return 1;
 
-// // Función para calcular la diferencia de stats entre dos Pokémon
-// function calculateScoreDifference(statsA, statsB) {
-//   var difference = 0;
-//   for (var stat in statsA) {
-//     difference += Math.abs(statsA[stat] - statsB[stat]);
-//   }
-//   return difference;
-// }
+    } else {
+      // Player Pokemon Loses
+      return 2;    
+    }
+  }
 
-// // Ejemplo de uso:
-// playerName = prompt("Ingresa tu nombre:");
-// console.log("Bienvenido(a), " + playerName + "!");
+  function compareResult(result,playerOption,buttonDiv) {
+    // Trigger action using the battle
+    // result
+    if (result == playerOption) {
+      playerActionResult(1,buttonDiv);
+    } else if(
+      result != playerOption || result != 0
+    ) {
+      playerActionResult(2,buttonDiv);
+    } else {
+      playerActionResult(0,buttonDiv);
+    }
+  }
 
-// // Mostrar información del Pokémon del jugador
-// console.log("Tu Pokémon:");
-// console.log("Nombre: " + playerPokemon.name);
-// console.log("Imagen: " + playerPokemon.image);
-// console.log("Stats: ", playerPokemon.stats);
+  function playerActionResult(choice,buttonDiv) {
+    // Execute action for player choice
+    const lifeSelector = document.querySelector("#player-life");
+    const scoreSelector = document.querySelector("#player-score");
+    
+      switch (choice) {
+          case 0:
+              displayBattleResult("Draw!");
+              displayRestartButton(buttonDiv);
+              return;
+          case 1:
+              displayBattleResult("You Win!");
+              playerScore += calculateScoreDifference( playerPokemon.stats, enemyPokemon.stats );
+              displayScore(scoreSelector,playerScore);
+              displayRestartButton(buttonDiv);
+              return;
+          case 2:
+              playerLives--;
+              playerScore -= calculateScoreDifference( playerPokemon.stats, enemyPokemon.stats );
 
-// // Mostrar información del Pokémon del enemigo
-// console.log("Pokémon del enemigo:");
-// console.log("Nombre: " + enemyPokemon.name);
-// console.log("Imagen: " + enemyPokemon.image);
-// console.log("Stats: ", enemyPokemon.stats);
+              if(playerLives <= 0) {
+                // Game Over situation
+                break;
+              }
 
-// // Simular la elección del jugador (puedes cambiar el número para probar diferentes resultados)
-// var playerChoice = parseInt(prompt("Elige: 1) Ataque, 2) Defensa, 3) Velocidad"));
-// battleResult(playerChoice);
+              displayBattleResult("You Lose!");
+              displayScore(scoreSelector,playerScore);
+              displayLife(lifeSelector,playerLives);
+              displayRestartButton(buttonDiv);
+              return;
+        };
+      
+      // Execute Game Over situation
+      playerScore -= calculateScoreDifference( playerPokemon.stats, enemyPokemon.stats );
+      displayScore(scoreSelector,playerScore);
+      displayLife(lifeSelector,playerLives);
+      displayGameOver();
+    
+  }
+
+  function displayGameOver(){
+    // Display Game Over and New Game button
+    displayBattleResult("GAME OVER");
+    const buttonSelector = document.querySelector("#action-buttons");
+    displayNewGameButton(buttonSelector);
+  }
+
+  function calculateScoreDifference(statsA, statsB) {
+    // Calculate difference between
+    // two stats
+    return Math.abs(statsA - statsB);
+  }
+
+  async function getNewPokemon(pokemon) {
+    // Get a New Pokemon and save
+    // into pokemon object
+      let newPokemon = await getRandomPokemon();
+      pokemon.name = newPokemon.name;
+      pokemon.image = newPokemon.sprites.front_default;
+      pokemon.stats = baseStatTotal(newPokemon);
+  }
+
+  function displayLife(selector, playerLives) {
+    // Render pokeball as life
+    selector.innerHTML = "";
+    let lifeHTML = "";
+    for (let i = 0; i < playerLives; i++) {
+        lifeHTML += '<img src="/images/pokeball.png" alt="Life">';
+      }
+    selector.innerHTML = lifeHTML;      
+  }
+
+  function displayScore(selector,playerScore) {
+    // Render actual Score
+      selector.querySelector("span").innerHTML = playerScore;
+  }
+
+  function displayStadium(selector,clean=true) {
+    // Render a Stadium
+    // True: Remove first element
+    // (Use it in New Game)
+    if(clean){
+      selector.firstElementChild.remove();
+    }
+      let stadiumNumber = Math.ceil(Math.random() * 4);
+      let stadiumBackground = `<img src="/images/stadium-${stadiumNumber}.jpg" alt="Stadium-${stadiumNumber}">`;
+      selector.insertAdjacentHTML("afterbegin",stadiumBackground);
+  }
+
+  function displayPokemon(selector,pokemon,rotate=true) {
+    // Render Pokemon
+      selector.innerHTML = "";
+
+      let newPokemonImg = document.createElement("img");
+
+      newPokemonImg.src = pokemon.image;
+      newPokemonImg.alt = pokemon.name;
+      if(rotate){
+          newPokemonImg.classList.add("player-pokemon");
+      };
+      
+      selector.appendChild(newPokemonImg);
+      
+  }
+
+  function displayBattleResult (result){
+    // Render battle result
+    // (also can be used for Game Over)
+    const selector = document.querySelector("#battle-result");
+    selector.innerHTML = `<h3>${result}</h3>`;
+  }
+
+}
